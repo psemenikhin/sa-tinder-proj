7e6921aa91ea74efcb5918154fa55f0fa2e32968
psemenikhin <petro.semenikhin@gmail.com>
2023-02-14 00:24:20 +0200

    Changed index

--- server/index.js
+++ server/index.js
@@ -12,8 +12,6 @@ const uri = process.env.URI
 
 const app = express()
 app.use(cors())
-app.use(express.json())
-
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', parameterLimit: 5000000}));
 // Default

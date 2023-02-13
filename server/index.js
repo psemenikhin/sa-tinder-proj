82bfd30681bb250fa3cff4c69e96555a35694ef2
psemenikhin <petro.semenikhin@gmail.com>
2023-02-14 00:07:35 +0200

    Update index

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', parameterLimit: 500000, extended: true}));
+app.use(express.urlencoded({limit: '50mb', parameterLimit: 5000000, extended: true}));
 // Default
 app.get('/', (req, res) =>
 {
@@ -161,8 +161,6 @@ app.put('/user', async (req, res) =>
 {
     const client = new MongoClient(uri)
     const formData = req.body.formData
-
-    console.log('Limit file size: '+ limit);
 
 try {
     await client.connect()

e09fe359c3c6da2c32be0284e76ef2d988f31aef
psemenikhin <petro.semenikhin@gmail.com>
2023-02-13 23:45:50 +0200

    Trying to solve 413

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', parameterLimit: 500000, extended: true}));
 // Default
 app.get('/', (req, res) =>
 {
@@ -159,9 +159,10 @@ try {
 
 app.put('/user', async (req, res) =>
 {
-const client = new MongoClient(uri)
-const formData = req.body.formData
+    const client = new MongoClient(uri)
+    const formData = req.body.formData
 
+    console.log('Limit file size: '+ limit);
 
 try {
     await client.connect()
